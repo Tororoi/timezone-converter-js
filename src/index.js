a405905c3d7f4ffb73e4f1abe224242bcb7e7bc3
Tororoi <cantwell.tom@gmail.com>
2020-09-21 13:45:16 -0400

    added moment

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ const incrementButton = document.getElementById('increment-button')
 incrementButton.addEventListener('click', () => {
   const currentNumber = Number.parseInt(output.innerText);
   output.innerText = currentNumber + 1;
+  let userZone = moment.tz.guess(true);
+  let zoneObj = moment.tz.zone(userZone);
+  console.log(zoneObj.parse(Date.UTC(2012, 2, 19, 8, 30)))
 });
